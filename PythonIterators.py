98023e443bb644daeaa3a714442de6abf9339d18
chisoftltd <chisoftltd@gmail.com>
2021-02-23 15:19:07 +0100

    Python Iterators

--- PythonIterators.py
+++ PythonIterators.py
@@ -36,6 +36,50 @@ print()
 
 mytuple1 =  "carrot"
 
-for x1 in mytuple1:
+for x1 in mytuple1 :
     print(x1)
 print()
+
+# Create an Iterator
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+    
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print()
+
+# StopIteration
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+    
+    def __next__(self):
+        if self.a <= 20:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+    print(x)
